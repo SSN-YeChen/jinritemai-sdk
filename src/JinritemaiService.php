32ec5af6bf8a5479c4676b1b57d5dd6968300d49
yeronghao <376600500@qq.com>
2020-11-13 11:39:26 +0800

    新增 获取已发货且有售后的订单列表 新增 根据子订单ID查询退款详情

--- src/JinritemaiService.php
+++ src/JinritemaiService.php
@@ -33,6 +33,11 @@ class JinritemaiService extends Common
     //订单详情
     const ORDER_DETAIL = 'order/detail';
 
+    //已发货且有售后的订单列表
+    const AFTER_SALE_ORDER_LIST = 'afterSale/orderList';
+    //根据子订单ID查询退款详情
+    const AFTER_SALE_REFUND_PROCESS_DETAIL = 'afterSale/refundProcessDetail';
+
     /**
      *  请求参数返回错误代码
 
@@ -295,6 +300,52 @@ class JinritemaiService extends Common
             'order_id' => $orderId . 'A'
         ];
         $this->makeParam($this->method(self::ORDER_DETAIL),$param);
+        $requestQuery = $this->getRequestQuery($apiUrl,$this->param);
+        return $requestQuery;
+    }
+
+
+    /**
+     * Notes: 获取已发货且有售后的订单列表
+     * @param $startTime 开始时间 2018/06/03 00:00:00
+     * @param $endTime 结束时间
+     * @param $type 类型(1.全部售后单 2.待商家处理 3.待商家收货 4.待客服仲裁 5.退款成功 7.待买家退货)
+     * @param int $page 页数（默认值为0，第一页从0开始）
+     * @param int $size 每页订单数（默认为10，最大100）
+     * @param string $orderBy 搜索时间条件：按订单创建时间create_time；按订单更新时间进行搜索update_time
+     * @param int $isDesc 订单排序方式：最近的在前，1；最近的在后，0
+     * @return mixed
+     * @throws Exception
+     */
+    public function getAfterSaleOrderList($startTime,$endTime,$type,$page = 1,$size = 100,$orderBy = 'create_time',$isDesc = 0){
+        $apiUrl = $this->host.self::AFTER_SALE_ORDER_LIST;
+        $param = [
+            'start_time' => $startTime,
+            'end_time' => $endTime,
+            'type' => $type,
+            'page' => $page,
+            'size' => $size,
+            'order_by' => $orderBy,
+            'is_desc' => $isDesc
+        ];
+        $this->makeParam($this->method(self::AFTER_SALE_ORDER_LIST),$param);
+        $requestQuery = $this->getRequestQuery($apiUrl,$this->param);
+        return $requestQuery;
+    }
+
+
+    /**
+     * Notes:根据子订单ID查询退款详情
+     * @param $orderId 子订单ID，不带字母A
+     * @return mixed
+     * @throws Exception
+     */
+    public function getAfterSaleRefundProcessDetail($orderId){
+        $apiUrl = $this->host.self::AFTER_SALE_REFUND_PROCESS_DETAIL;
+        $param = [
+            'order_id' => $orderId
+        ];
+        $this->makeParam($this->method(self::AFTER_SALE_REFUND_PROCESS_DETAIL),$param);
         $requestQuery = $this->getRequestQuery($apiUrl,$this->param);
         return $requestQuery;
     }
